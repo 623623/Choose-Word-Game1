3ed095cf56506f7115109c1c7214cea874872e8f
Anton Povelicin <34660886+623623@users.noreply.github.com>
2019-11-16 15:01:00 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -59,3 +59,49 @@ function func2() {
         alert("please select answer");
     }
 }
+function func3() {
+    var radio5 = document.getElementById("radio9");
+    var radio6 = document.getElementById("radio10");
+    var radio7 = document.getElementById("radio11");
+    var radio8 = document.getElementById("radio12");
+    var correct2 = false;
+
+    if (radio9.checked) {
+        correct = false;
+        alert("");
+        console.log("radio9 is checked")
+    }
+    else if (radio10.checked){
+        correct2 = false;
+        alert("radio10 is checked");
+        console.log("radio6 is checked")
+    }
+    else if (radio11.checked){
+        correct2 = false;
+        alert("radio11 is checked");
+        console.log("radio7 is checked")
+    }
+    else if (radio12.checked){
+        correct2 = true;
+        alert("radio12 is checked");
+        console.log("radio8 is checked")
+    }
+    else{
+        alert("please select answer");
+    }
+}
+window.onload = function func4() {
+
+    
+
+    p = document.getElementById("result").innerHTML = 222;
+
+    if (radio12.checked) {
+        innerHTML = 3;
+    }
+
+   
+    
+    
+    
+}
